f6c473a890ba44a6e7bd501cf0b990885f94974e
richie060 <richuhi060@gmail.com>
2022-03-10 01:21:55 -0800

    first commit

---
+++ register.php
@@ -0,0 +1,41 @@
+<?php include ('server.php');?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Register</title> 
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div class="header">
+		Register
+	</div>
+	<form method="post" action="register.php">
+
+		<?php include('errors.php'); ?>
+	<div class="input-group">
+
+		<label>Username</label>
+		<input type="text" name="username" value="<?php echo $username?>">
+	</div>
+		<div class="input-group">
+			<label>Email</label>
+			<input type="text" name="email" value="<?php echo $email?>">
+		</div>
+	<div class="input-group">
+			<label>Password</label>
+			<input type="password" name="password_1" >
+		</div>
+		<div class="input-group">
+			<label>Confirm password</label>
+			<input type="password" name="password_2">
+		</div>
+		<div class="input-group">
+			<button type="submit" name="register" class="btn">Register</button>
+		</div>
+		<p>Already a member?<a href="login.php">Sign in</p>
+	</form>
+
+</body>
+</html>
